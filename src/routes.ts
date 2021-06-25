dc37fa5c1b659c4175f6038c7d1532afa3982765
fabio-ericle <fabioericle@gmail.com>
2021-06-25 01:15:57 -0300

    In Orbit

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUsersController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
+const createTagController = new CreateTagController();
 const createUserController = new CreateUserController();
 
-router.post("/users", (createUserController.handle));
+router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/users", createUserController.handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 //Day 1 code: #together
 //Day 2 code: #unidade
+//Day 3 code: #embuscadeevolução
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors"
 
 import { router } from "./routes";
 
@@ -11,5 +13,18 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    });
+  }
+  
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(3000, () => console.log("Server is running"));
